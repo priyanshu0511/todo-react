31663d9c2e1a1e0c17d5f1e0c6cd06a48dff50d0
Priyanshu Singh <priyanshu05112@gmail.com>
2024-07-03 22:54:37 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,113 @@
-import logo from './logo.svg';
-import './App.css';
+import { FaEdit } from "react-icons/fa";
+import Navbar from "./components/Navbar";
+import { AiFillDelete } from "react-icons/ai";
+import { useEffect, useState } from "react";
+import { v4 as uuidv4 } from 'uuid';
+
+
 
 function App() {
+
+  const [todo, setTodo] = useState("");
+  const [todos, setTodos] = useState([]);
+  const [finished,setFinished]=useState(true);
+
+  useEffect(()=>{
+    let todoString=localStorage.getItem("todos");
+    if(todoString !== null){
+      let todos=JSON.parse(localStorage.getItem("todos"))
+    setTodos(todos)
+    }
+  },[])
+
+  const saveToLS = () =>{
+    localStorage.setItem('todos',JSON.stringify(todos))
+  }
+
+  const handleDelete = (e,id) => {
+    let newTodos=todos.filter(item =>{
+      return item.id!==id;
+    })
+    setTodos(newTodos);
+    saveToLS();
+  }
+
+  const handleEdit = (e,id) => {
+    let todo=todos.filter(item =>{
+      return item.id===id;
+    })
+    setTodo(todo[0].todo);
+    let newTodos=todos.filter(item =>{
+      return item.id!==id;
+    })
+    setTodos(newTodos);
+    saveToLS();
+  }
+
+  const handleAdd = () => {
+    setTodos([...todos, {id: uuidv4(), todo, isCompleted: false }])
+    setTodo("");
+    saveToLS();
+  }
+
+  const handleChange = (e) => {
+    setTodo(e.target.value)
+  }
+
+  const handleCheckbox =(e)=>{
+    let id=e.target.name
+    let index=todos.findIndex(item =>{
+      return item.id === id
+    }
+    )
+    let newTodos=[...todos];
+    newTodos[index].isCompleted=!newTodos[index].isCompleted;
+    setTodos(newTodos);
+    saveToLS();
+  }
+
+  const toggleFinished=()=>{
+    setFinished(!finished)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    < >
+      <Navbar />
+      <div className="mx-3 md:container md:mx-auto my-5 rounded-xl p-5 bg-violet-100 min-h-[80vh] md:w-[35%]">
+        <h1 className='font-bold text-center text-3xl'>iTask - Manage your todos at one place</h1>
+        <div className="addTodo my-5 flex flex-col gap-4">
+          <h2 className='text-2xl font-bold'>Add a Todo</h2>
+          <div className="flex">
+
+            <input type="text" onChange={handleChange} value={todo} className='w-full rounded-full px-5 py-1' />
+            <button disabled={todo.length<3} onClick={handleAdd} className='bg-violet-800 mx-2 rounded-full hover:bg-violet-950 disabled:bg-violet-500 p-4 py-2 text-sm font-bold text-white'>Save</button>
+          </div>
+        </div>
+        <input className='my-4' checked={finished} onChange={toggleFinished} id='show' type="checkbox" />
+        <label className='mx-2' htmlFor="show">Show Finished</label>
+        <div className='h-[1px] bg-black opacity-15 w-[90%] mx-auto my-2'></div>
+        <h2 className='text-2xl font-bold'>Your Todos</h2>
+        <div className="todos">
+          {todos.length === 0 && <div className="m-5">No todos found!!</div>}
+          {todos.map((item) => {
+            return(finished || item.isCompleted===false) && (<div key={item.id} className="flex w-full justify-between">
+              <div className='flex gap-5'>
+                <input type="checkbox" name={item.id} onChange={handleCheckbox} id="" checked={item.isCompleted}/>
+                <div className={item.isCompleted ? "line-through":""}>{item.todo}</div>
+              </div>
+              <div className="buttons flex h-full">
+                <button onClick={(e)=>handleEdit(e,item.id)} className='bg-violet-800 hover:bg-violet-950 p-2 py-1 text-sm font-bold text-white rounded-md mx-1'><FaEdit /></button>
+                <button onClick={(e)=>handleDelete(e,item.id)} className='bg-violet-800 hover:bg-violet-950 p-2 py-1 text-sm font-bold text-white rounded-md mx-1'><AiFillDelete /></button>
+              </div>
+            </div>
+            )
+          })}
+        </div>
+      </div>
+
+
+    </>
+  )
 }
 
 export default App;
